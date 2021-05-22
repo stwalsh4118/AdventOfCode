2049f721a179b7ebf7f785f9abf367d25fd564b1
stwalsh4118 <40175938+stwalsh4118@users.noreply.github.com>
2021-05-22 14:36:11 -0500

    AdventOfCode in C 2015-Day2-NoMath Part2

--- 2015/Day2 NoMath/NoMath.c
+++ 2015/Day2 NoMath/NoMath.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include "../../headers/utils.h"
 
-#define ARRAYLENGTH(x) sizeof(x)/sizeof(x[0])
+//box struct that holds the length width and height of the box
 typedef struct {
     int length;
     int height;
@@ -13,9 +13,14 @@ typedef struct {
 
 
 void extract_dimensions(char *raw_dim, int dimensions[]) {
+    //split the dimensions string that we got from the file at the x
     char * token = strtok(raw_dim,"x");
     int index = 0;
+
+    //keep splitting until we get to the end of the dimensions
     while(token != NULL) {
+
+        //convert the string version of the dimension into an int then store it in the dimensions array to use later
         dimensions[index++] = atoi(token);
         token = strtok(NULL, "x");
     }
@@ -25,25 +30,33 @@ BOX *create_boxes(int num_boxes) {
 
     FILE* fp = fopen("boxes.txt", "r");
 
+    //allocate memory for each set of dimensions in the file 
     BOX *boxes = (BOX*)malloc(num_boxes * sizeof(BOX));
     
     char * box;
 
     int index = 0;
 
+    //while there are still boxes to be read from the file
     while(box=input_string(fp,10)) {
         
         if(strcmp(box,"") == 0) {
             break;
         }
+
         printf("%s\n",box);
         int dimensions[3];
+
+        //change the string representation of the dimensions into ints that we can use
         extract_dimensions(box, dimensions);
 
+        //store the dimensions in their respective box object
         boxes[index].length = dimensions[0];
         boxes[index].width = dimensions[1];
         boxes[index].height = dimensions[2];
         index++;
+
+        free(box);
     }   
 
     fclose(fp);
@@ -56,8 +69,11 @@ int calculate_surface_area(BOX box) {
     int HW = box.height*box.width;
     int LW = box.length*box.width;
 
+    //calculate the surface area of the box
     int surface_area = 2*(HL + HW + LW);
     
+
+    //calculate the smallest side of the box to find out the amount of slack we need
     if(HL <= HW && HL <= LW) {
         surface_area += HL;
     } else if (HW <= HL && HW <= LW) {
@@ -69,10 +85,31 @@ int calculate_surface_area(BOX box) {
     return surface_area;
 }
 
+int caluclate_volume(BOX box) {
+    //calculate the volume of the box is cubic feet
+    return box.height*box.width*box.length;
+}
+
+int calculate_smallest_perimeter(BOX box) {
+    int HL = box.height + box.length;
+    int HW = box.height + box.width;
+    int LW = box.length + box.width;
+
+    //check for the smallest side then return its perimeter
+    if(HL <= HW && HL <= LW) {
+        return 2*HL;
+    } else if (HW <= HL && HW <= LW) {
+        return 2*HW;
+    } else {
+        return 2*LW;
+    }
+}
+
 long calculate_total_paper(BOX *boxes, size_t num_boxes) {
     
     long total_paper = 0;
-
+    
+    //loop through all the boxes adding up their surface areas + slack needed
     for(int i = 0; i < num_boxes; i++) {
         total_paper += calculate_surface_area(boxes[i]);
     }
@@ -81,11 +118,28 @@ long calculate_total_paper(BOX *boxes, size_t num_boxes) {
 
 }
 
+long calculate_total_ribbon(BOX *boxes, size_t num_boxes) {
+
+    long total_ribbon = 0;
+
+    //loop through all the boxes adding up the perimeter of their smallest side and their volume
+    for(int i = 0; i < num_boxes; i++) {
+        total_ribbon += (calculate_smallest_perimeter(boxes[i]) + caluclate_volume(boxes[i]));
+    }
+
+    return total_ribbon;
+}
+
 int main(int argc, char * argv[]) {
 
-    BOX *boxes;
+    printf("Input file path to box dimensions: ");
 
-    FILE* fp = fopen("boxes.txt", "r");
+    char *file_path;
+    file_path = input_string(stdin, 16);
+
+    FILE *fp = fopen(file_path, "r");
+
+    BOX *boxes;
 
     int num_boxes = count_lines(fp);
     fclose(fp);
@@ -94,6 +148,9 @@ int main(int argc, char * argv[]) {
     boxes = create_boxes(num_boxes);
 
     
-    printf("total paper needed %ld", calculate_total_paper(boxes, num_boxes));
+    printf("total paper needed %ld\n", calculate_total_paper(boxes, num_boxes));
+    printf("total ribbon needed %ld\n", calculate_total_ribbon(boxes, num_boxes));
+
+    free(boxes);
     return 0;
 }
---
+++ 2015/Day2 NoMath/README.txt
@@ -0,0 +1,11 @@
+To run program:
+
+Run executable
+
+Input string that corresponds with your box dimensions (one per line, each dimension seperated by an 'x', e.g., 12x22x87)
+
+Press Enter
+
+Output will be:
+    the amount of wrapping paper needed for each box (the surface area plus the area of the smallest side for some slack)
+    the amount of ribbon needed to wrap the present plus a bow (calculated by the side with the smallest perimeter plus the volume of the box) 
