9a99f018246414455915b20a262b0b63bb2865f0
stwalsh4118 <40175938+stwalsh4118@users.noreply.github.com>
2021-05-21 22:44:40 -0500

    AdventOfCode in C, 2015-Day1-NotQuiteLisp Part2

--- 2015/NotQuiteLisp.c
+++ 2015/NotQuiteLisp.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 
 char * input_string(FILE* fp, size_t initial_size) {
@@ -44,17 +45,23 @@ char * input_string(FILE* fp, size_t initial_size) {
 
 }
 
-int get_floor(char * instructions) {
+void get_floor(char * instructions, int answer[]) {
     int floor = 0;
     char left_parenthesis = '(';
+    bool has_entered_basement = false;
      
     //loop through input
     for(int i = 0; i < strlen(instructions); i++) {
 
-        //if at the end of the instructions break from the loop
+        //if at the end of the instructions breadk from the loop
+        if(!(has_entered_basement) && (floor==-1)) {
+            answer[1] = i;
+            has_entered_basement = true;
+        }
+
+
         if(instructions[i] == 0) {
             break;
-
         //if current character is a left_parenthesis go up a floor
         } else if(instructions[i] == left_parenthesis) {
             floor++;
@@ -66,7 +73,7 @@ int get_floor(char * instructions) {
     }
 
     //return the floor number we end up at
-    return floor;
+    answer[0] = floor;
 
 }
 
@@ -85,10 +92,15 @@ int main(int argc, char * argv[]) {
 
     printf("\nInputted instructions: %s\n", instructions);
 
-    int floors = get_floor(instructions);
+    int answer[2];
+    get_floor(instructions, answer);
 
-    printf("Floor ended on: %d\n", floors);
-    
+    printf("Floor ended on: %d\n", answer[0]);
+    if(answer[1] != 0) {
+        printf("Instruction basement was entered at: %d\n", answer[1]);
+    } else {
+        printf("Never entered the basement!\n");
+    }
 
     free(instructions);
     return 0;
