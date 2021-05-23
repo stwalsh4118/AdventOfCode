070d699f3b7add652bc6c904541e2e66f6feac85
stwalsh4118 <40175938+stwalsh4118@users.noreply.github.com>
2021-05-23 15:10:42 -0500

    AdventOfCode in C 2015-Day6-FireHazard Part2

--- 2015/Day6_FireHazard/FireHazard.c
+++ 2015/Day6_FireHazard/FireHazard.c
@@ -6,6 +6,7 @@
 
 int * parse_command(char * command) {
 
+    //allocate memory for the parsed commands so we can return this 
     int * commands = (int *) malloc(5 * sizeof(int));
 
     char * token = strtok(command, " ");
@@ -49,6 +50,7 @@ int * parse_command(char * command) {
 int count_lights(bool lights[1000][1000]) {
     int lit_lights = 0;
 
+    //loop through all of the lights and count them if they are on
     for(int i = 0; i < 1000; i++) {
         for(int j = 0; j < 1000; j++) {
             if(lights[i][j]) {
@@ -59,8 +61,23 @@ int count_lights(bool lights[1000][1000]) {
     return lit_lights;
 }
 
+long count_brightness(int ** brightness) {
+    long total_brightness = 0;
+
+    //loop through all of the lights and add up their brightness values
+    for(int i = 0; i < 1000; i++) {
+        for(int j = 0; j < 1000; j++) {
+            total_brightness += brightness[i][j];
+        }
+    }
+
+    return total_brightness;
+}
+
 void turn_on_lights(bool lights[1000][1000], int x1, int y1, int x2, int y2) {
 
+
+    //turn on the lights within the given range
     for(int i = x1; i <= x2; i++) {
         for(int j = y1; j <= y2; j++) {
             lights[i][j] = true;
@@ -70,6 +87,8 @@ void turn_on_lights(bool lights[1000][1000], int x1, int y1, int x2, int y2) {
 }
 
 void turn_off_lights(bool lights[1000][1000], int x1, int y1, int x2, int y2) {
+
+    //turn off the lights within the given range
     for(int i = x1; i <= x2; i++) {
         for(int j = y1; j <= y2; j++) {
             lights[i][j] = false;
@@ -78,6 +97,8 @@ void turn_off_lights(bool lights[1000][1000], int x1, int y1, int x2, int y2) {
 }
 
 void toggle_lights(bool lights[1000][1000], int x1, int y1, int x2, int y2) {
+
+    //toggle the lighs within the given range
     for(int i = x1; i <= x2; i++) {
         for(int j = y1; j <= y2; j++) {
             lights[i][j] = !lights[i][j];
@@ -85,45 +106,145 @@ void toggle_lights(bool lights[1000][1000], int x1, int y1, int x2, int y2) {
     }
 }
 
-int do_commands(FILE* fp) {
+void turn_up_lights(int ** brightness, int x1, int y1, int x2, int y2) {
+
+    //turn up the brightness of the lights by one within the given range
+    for(int i = x1; i <= x2; i++) {
+        for(int j = y1; j <= y2; j++) {
+            brightness[i][j]++;
+        }
+    }
+
+}
+
+void turn_down_lights(int ** brightness, int x1, int y1, int x2, int y2) {
+
+    //turn down the brightness of the lights by one within the given range (as long as they aren't 0)
+    for(int i = x1; i <= x2; i++) {
+        for(int j = y1; j <= y2; j++) {
+            if(brightness[i][j] != 0) {
+                brightness[i][j]--;
+            }
+        }
+    }
+}
+
+void turn_up_twice(int ** brightness, int x1, int y1, int x2, int y2) {
+
+    //turn up the brightness of the lights by two within the given range
+    for(int i = x1; i <= x2; i++) {
+        for(int j = y1; j <= y2; j++) {
+            brightness[i][j]+=2;
+        }
+    }
+}
+
+int do_commands_bool(FILE* fp) {
+
 
     char * command;
     int * parsed_command;
 
     bool lights[1000][1000];
     
+
+    //initialize the lights array to all falses to start "off"
     for(int i = 0; i < 1000; i++) {
         for(int j = 0; j < 1000; j++) {
             lights[i][j] = false;
         }
     }
 
+
+
+    
+    //loop through all of the commands
+    while(command = input_string(fp,10)) {
+
+        //if the command is an empty string that means we hit the end of the file and we should break from the loop
+        if(strcmp(command, "") == 0) {
+            break;
+        }
+
+
+        //parse the command into its seperate parts
+        parsed_command = parse_command(command);
+
+
+        //if the command contains "turn on"
+        if(parsed_command[0] == 1) {
+            turn_on_lights(lights, parsed_command[1], parsed_command[2], parsed_command[3], parsed_command[4]);
+
+        // if the command contains "turn off"
+        } else if(parsed_command[0] == 2) {
+            turn_off_lights(lights, parsed_command[1], parsed_command[2], parsed_command[3], parsed_command[4]);
+
+        // if the command contains "toggle"
+        } else {
+            toggle_lights(lights, parsed_command[1], parsed_command[2], parsed_command[3], parsed_command[4]);
+        }
+
+
+        //free the memory that was allocated
+        free(parsed_command);
+        free(command);
+    }
+
+
+    printf("lights on %d\n",count_lights(lights));
+
+
+    return 0;
+}
+
+int do_commands_int(FILE* fp) {
+
+
+
+    char * command;
+    int * parsed_command;
+
+    //allocate memory to hold the pointers to the "rows"
+    int ** brightness = (int **) malloc (1000 * sizeof(int*));
+
+    //allocate memory for each "row" and add it to the array of arrays
+    for(int i = 0; i < 1000; i++) {
+        brightness[i] = (int *) malloc (1000* sizeof(int));
+    }
+    
+    //initialize all values within the array to 0 
+    for(int i = 0; i < 1000; i++) {
+        for(int j = 0; j < 1000; j++) {
+            brightness[i][j] = 0;
+        }
+    }
+
+    //rest is same as above
     while(command = input_string(fp,10)) {
 
         if(strcmp(command, "") == 0) {
             break;
         }
 
-
         parsed_command = parse_command(command);
 
         if(parsed_command[0] == 1) {
-            printf("turn on lights\n");
-            turn_on_lights(lights, parsed_command[1], parsed_command[2], parsed_command[3], parsed_command[4]);
+            turn_up_lights(brightness, parsed_command[1], parsed_command[2], parsed_command[3], parsed_command[4]);
         } else if(parsed_command[0] == 2) {
-            printf("turn off lights\n");
-            turn_off_lights(lights, parsed_command[1], parsed_command[2], parsed_command[3], parsed_command[4]);
+            turn_down_lights(brightness, parsed_command[1], parsed_command[2], parsed_command[3], parsed_command[4]);
         } else {
-            printf("toggle lights\n");
-            toggle_lights(lights, parsed_command[1], parsed_command[2], parsed_command[3], parsed_command[4]);
+            turn_up_twice(brightness, parsed_command[1], parsed_command[2], parsed_command[3], parsed_command[4]);
         }
+        
+
 
         free(parsed_command);
-        free(command);
+        free(command); 
+
     }
 
-    printf("lights on %d\n",count_lights(lights));
-
+    printf("total brightness %d\n",count_brightness(brightness));
+    free(brightness);
 
     return 0;
 }
@@ -135,9 +256,19 @@ int main(int argc, char * argv[]) {
     char *file_path;
     file_path = input_string(stdin, 16);
 
+    printf("file path %s\n",file_path);
     FILE *fp = fopen(file_path, "r");
 
-    do_commands(fp);
+    if(fp != NULL) {
+        printf("file opened correctly\n");
+    }
+
+    do_commands_bool(fp);
+
+    fclose(fp);
+    fp = fopen(file_path, "r");
+
+    do_commands_int(fp);
 
     fclose(fp);
     free(file_path);
---
+++ 2015/Day6_FireHazard/README.txt
@@ -0,0 +1,18 @@
+Insights:
+    This one was also fairly easy to conceptualize
+    Had difficulty with memory allocation of the 2d array, had to use malloc to allocate memory on the heap instead of the stack, apparently trying to initialize 1 million ints on the stack is a bad idea
+    Learned how to make multidimensional arrays with pointers using an array of pointers to hold all of the arrays
+    Learned more about how to pass arrays to functions
+    Learned more about string manipulation, mostly splitting strings on delimiters
+
+To run program:
+
+Run executable
+
+Input string that corresponds with your command list (strings in the form of "turn on/turn off/toggle x1,y1 through x2,y2" seperated by new lines)
+
+Press Enter
+
+Output will be:
+    the total number of lights that are switched on after all of the commands are run
+    the total brightnes of the lights after all the commands are run
